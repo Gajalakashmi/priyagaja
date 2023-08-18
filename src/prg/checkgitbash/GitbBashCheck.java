ab7a8a3999d377ec20795e47f44a2d9ece931f55
Gajalakshmi <gajoo96@gmail.com>
2023-08-18 13:36:56 +0530

    qa environment

--- src/prg/checkgitbash/GitbBashCheck.java
+++ src/prg/checkgitbash/GitbBashCheck.java
@@ -4,7 +4,8 @@ public class GitbBashCheck {
 
 	public static void main(String[] args) {
 		System.out.println("Master copy");
-
+		System.out.println("QA environment");
+		System.out.println("Code is working fine");
 	}
 
 }
